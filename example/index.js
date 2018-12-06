bc29e87466d1d57eb6e49cc859e2125ef0deafed
jesseokeya <jesseokeya@gmail.com>
2018-12-05 23:28:57 -0500

    changes

--- example/index.js
+++ example/index.js
@@ -1,5 +1,6 @@
 const Infermedica = require('../index')
 const infermedica = new Infermedica({ appId: '72c76576', appKey: '2c63689dc7de4d0cab1967cb1cd09ef7' })
+
 infermedica.getConditions().then(res => {
     console.log(res.data)
 })
